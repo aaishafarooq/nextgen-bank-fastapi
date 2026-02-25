e64995107559e9e6ec3c21a8adfe45eca030207a
Aaisha Farooq <aaishafarooq01@gmail.com>
2026-02-25 10:14:28 +0530

    chore: install celery and email related packages

--- backend/app/api/routes/home.py
+++ backend/app/api/routes/home.py
@@ -9,9 +9,4 @@ router = APIRouter(prefix="/home")
 
 @router.get("/")
 def home():
-    logger.info("home page accessed")
-    logger.debug("home page accessed")
-    logger.error("home page accessed")
-    logger.warning("home page accessed")
-    logger.critical("home page accessed")
     return {"message": "Welcome to the NextGen Bank API"}
