7cb8d7b519db3e38fb5b8d67598364dc9ed3800a
Aaisha Farooq <aaishafarooq01@gmail.com>
2026-03-10 16:10:45 +0530

    feat: add custom token auth config and utils.

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import Literal
 class Settings(BaseSettings):
-   
+    # TODO: change to production when time comes
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
 
     model_config = SettingsConfigDict(
@@ -35,5 +35,18 @@ class Settings(BaseSettings):
     SUPPORT_EMAIL: str = ""
     JWT_SECRET_KEY: str = ""
     JWT_ALGORITHM: str = "HS256"
+
+    JWT_ACCESS_TOKEN_EXPIRATION_MINUTES: int = 30 if ENVIRONMENT == "local" else 15
+    JWT_REFRESH_TOKEN_EXPIRATION_DAYS: int = 1
+    COOKIE_SECURE: bool = False if ENVIRONMENT == "local" else True
+    COOKIE_ACCESS_NAME: str = "access_token"
+    COOKIE_REFRESH_NAME: str = "refresh_token"
+    COOKIE_LOGGED_IN_NAME: str = "logged_in"
+
+    COOKIE_HTTP_ONLY: bool = True
+    COOKIE_SAMESITE: str = "lax"
+    COOKIE_PATH: str = "/"
+    SIGNING_KEY: str = ""
+
 
 settings = Settings()
