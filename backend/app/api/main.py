8401c7a70f99e36c06e4ab501fa66e305b127ddb
Aaisha Farooq <aaishafarooq01@gmail.com>
2026-03-13 15:36:50 +0530

    feat: logout user and clear all cookies.

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -3,6 +3,7 @@ from backend.app.api.routes import home
 from backend.app.api.routes.auth import (
     activate,
     login,
+    logout,
     password_reset,
     refresh,
     register,
@@ -16,3 +17,4 @@ api_router.include_router(activate.router)
 api_router.include_router(login.router)
 api_router.include_router(password_reset.router)
 api_router.include_router(refresh.router)
+api_router.include_router(logout.router)
