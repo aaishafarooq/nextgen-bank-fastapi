cad90bdb8dd0ef0504f6913b361b67bd15ac4d7c
Aaisha Farooq <aaishafarooq01@gmail.com>
2026-03-15 14:40:30 +0530

    ref: refactor the celery tasks location.

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,5 +1,6 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import Literal
+import cloudinary
 class Settings(BaseSettings):
     # TODO: change to production when time comes
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
@@ -49,5 +50,19 @@ class Settings(BaseSettings):
     SIGNING_KEY: str = ""
     PASSWORD_RESET_TOKEN_EXPIRATION_MINUTES: int = 3 if ENVIRONMENT == "local" else 5
 
+    CLOUDINARY_CLOUD_NAME: str = ""
+    CLOUDINARY_API_KEY: str = ""
+    CLOUDINARY_API_SECRET: str = ""
+
+    ALLOWED_MIME_TYPES: list[str] = ["image/jpeg", "image/png", "image/jpg"]
+    MAX_FILE_SIZE: int = 5 * 1024 * 1024
+    MAX_DIMENSION: int = 4096
+
 
 settings = Settings()
+
+cloudinary.config(
+    cloud_name=settings.CLOUDINARY_CLOUD_NAME,
+    api_key=settings.CLOUDINARY_API_KEY,
+    api_secret=settings.CLOUDINARY_API_SECRET,
+)
