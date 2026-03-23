3a9dd69d0c5f5374655f38ea97133c58147db055
Aaisha Farooq <aaishafarooq01@gmail.com>
2026-03-24 03:11:34 +0530

    feat: user can create next of kin.

--- backend/app/auth/models.py
+++ backend/app/auth/models.py
@@ -11,6 +11,7 @@ from backend.app.auth.schema import BaseUserSchema, RoleChoicesSchema
 
 if TYPE_CHECKING:
     from backend.app.user_profile.models import Profile
+    from backend.app.next_of_kin.models import NextOfKin
 
 class User(BaseUserSchema, table=True):
     id: uuid.UUID = Field(
@@ -54,6 +55,8 @@ class User(BaseUserSchema, table=True):
         },
     )
 
+    next_of_kins: list["NextOfKin"] = Relationship(back_populates="user")
+    
     @computed_field
     @property
     def full_name(self) -> str:
