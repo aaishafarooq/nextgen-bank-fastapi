97de250ad1ba6b790abfaa510e78f703efd969a6
Aaisha Farooq <aaishafarooq01@gmail.com>
2026-03-24 11:35:43 +0530

    feat: add bank account utils and account created email templates.

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -59,6 +59,15 @@ class Settings(BaseSettings):
     MAX_DIMENSION: int = 4096
 
 
+    BANK_CODE: str = ""
+    BANK_BRANCH_CODE: str = ""
+    CURRENCY_CODE_USD: str = ""
+    CURRENCY_CODE_EUR: str = ""
+    CURRENCY_CODE_GBP: str = ""
+    CURRENCY_CODE_KES: str = ""
+    MAX_BANK_ACCOUNTS: int = 3
+
+
 settings = Settings()
 
 cloudinary.config(
